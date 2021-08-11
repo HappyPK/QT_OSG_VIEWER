b189520ea1a00e39972723752d32c00edd26cac3
pengk <hari_pk@163.com>
2021-08-11 23:56:51 +0800

    update

--- src/Proj_1/mainwindow/MainWindow.cpp
+++ src/Proj_1/mainwindow/MainWindow.cpp
@@ -1,11 +1,9 @@
 #include "MainWindow.h"
-#include "testWidget.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    testWidget* testW = new testWidget(this);
     
     connect(ui->closeMainWindow, &QPushButton::clicked, this, &QMainWindow::close);
 }
--- src/project_1.cpp
+++
@@ -1,7 +0,0 @@
-#include "project_1.h"
-
-Project_1::Project_1(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
-}
